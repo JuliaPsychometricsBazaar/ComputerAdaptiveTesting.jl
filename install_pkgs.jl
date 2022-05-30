2faddca22ccc66b10bce373e6864db1cbc6bbbfd
Frankie Robertson <frankie@robertson.name>
2022-05-30 16:34:09 +0300

    skipcat for tests

--- install_pkgs.jl
+++ install_pkgs.jl
@@ -1,6 +1,11 @@
 using Pkg
-Pkg.develop(path=@__DIR__)
+if !(length(ARGS) >= 2 && ARGS[2] == "skipcat")
+  Pkg.develop(path=@__DIR__)
+end
 using ComputerAdaptiveTesting: install_extra
 
 install_extra(length(ARGS) > 0 && ARGS[1] == "headless" ? "all_headless" : "all")
-Pkg.add(url="https://github.com/JuliaMath/QuadGK.jl.git", rev="298f76e71be8a36d6e3f16715f601c3d22c2241c")
+Pkg.add(
+  url="https://github.com/JuliaMath/QuadGK.jl.git",
+  rev="298f76e71be8a36d6e3f16715f601c3d22c2241c"
+)
