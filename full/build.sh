4c6b946f0feed7f652f0187a856f5c102af57674
Frankie Robertson <frankie@robertson.name>
2022-05-30 10:56:15 +0300

    Fix build script for full env

--- full/build.sh
+++ full/build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 julia --project=. ../install_pkgs.jl
+
+cat << HEADER > Project.toml.tmp
+name = "ComputerAdaptiveTestingFull"
+uuid = "dcfddea1-f665-4ffb-8979-b04b0e81fb28"
+authors = ["Frankie Robertson"]
+version = "0.1.0"
+
+$(cat Project.toml)
+HEADER
+
+mv Project.toml.tmp Project.toml
