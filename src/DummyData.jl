0413eff9aa7655fbcce1871d3959e71b1e73aaf4
Frankie Robertson <frankie@robertson.name>
2022-07-02 16:31:10 +0300

    Add RNG argument to DummyData functions

--- src/DummyData.jl
+++ src/DummyData.jl
@@ -3,8 +3,11 @@ module DummyData
 using Distributions: Normal, MvNormal, ZeroMeanIsoNormal, Zeros, ScalMat
 using Faker: sentence
 using Random
+using StatsBase: sample
 
-using ..ItemBanks: ItemBank2PL, ItemBank3PL, ItemBankMirt4PL, ItemResponse
+using ..Responses: BareResponses
+using ..ItemBanks: AbstractItemBank, ItemBank2PL, ItemBank3PL, ItemBankMirt4PL, ItemResponse
+using ..Aggregators: NullAbilityTracker, TrackedResponses
 
 const default_num_questions = 8000
 const default_num_testees = 30
@@ -18,42 +21,45 @@ function sent()
     sentence(number_words=12, variable_nb_words=true)
 end
 
-function dummy_2pl_item_bank(num_questions)
+function dummy_2pl_item_bank(rng, num_questions)
     discrim_normal = Normal(1.0, 0.2)
-    difficulties = rand(std_normal, num_questions)
-    discriminations = abs.(rand(discrim_normal, num_questions))
+    difficulties = rand(rng, std_normal, num_questions)
+    discriminations = abs.(rand(rng, discrim_normal, num_questions))
     ItemBank2PL(difficulties, discriminations)
 end
+dummy_2pl_item_bank(num_questions) = dummy_2pl_item_bank(Random.default_rng(), num_questions)
 
-function dummy_3pl_item_bank(num_questions)
+function dummy_3pl_item_bank(rng, num_questions)
     discrim_normal = Normal(1.0, 0.2)
     guess_normal = Normal(0.0, 0.2)
-    difficulties = rand(std_normal, num_questions)
-    discriminations = abs.(rand(discrim_normal, num_questions))
-    guesses = clamp.(rand(guess_normal, num_questions), 0, 1)
+    difficulties = rand(rng, std_normal, num_questions)
+    discriminations = abs.(rand(rng, discrim_normal, num_questions))
+    guesses = clamp.(rand(rng, guess_normal, num_questions), 0, 1)
     ItemBank3PL(difficulties, discriminations, guesses)
 end
+dummy_3pl_item_bank(num_questions) = dummy_3pl_item_bank(Random.default_rng(), num_questions)
 
-function dummy_mirt_4pl_item_bank(num_questions, dims)
+function dummy_mirt_4pl_item_bank(rng, num_questions, dims)
     discrim_normal = Normal(1.0, 0.2)
     guess_normal = Normal(0.0, 0.2)
     slip_normal = Normal(0.0, 0.2)
-    difficulties = rand(std_normal, num_questions)
-    discriminations = abs.(rand(discrim_normal, dims, num_questions))
-    guesses = clamp.(rand(guess_normal, num_questions), 0.0, 1.0)
-    slips = clamp.(rand(slip_normal, num_questions), 0.0, 1.0)
+    difficulties = rand(rng, std_normal, num_questions)
+    discriminations = abs.(rand(rng, discrim_normal, dims, num_questions))
+    guesses = clamp.(rand(rng, guess_normal, num_questions), 0.0, 1.0)
+    slips = clamp.(rand(rng, slip_normal, num_questions), 0.0, 1.0)
     ItemBankMirt4PL(difficulties, discriminations, guesses, slips)
 end
+dummy_mirt_4pl_item_bank(num_questions) = dummy_mirt_4pl_item_bank(Random.default_rng(), num_questions)
 
-function item_bank_to_full_dummy(item_bank, num_testees)
-    (item_bank, question_labels, abilities, responses) = mirt_item_bank_to_full_dummy(item_bank, num_testees, 1; squeeze=true)
+function item_bank_to_full_dummy(rng, item_bank, num_testees)
+    (item_bank, question_labels, abilities, responses) = mirt_item_bank_to_full_dummy(rng, item_bank, num_testees, 1; squeeze=true)
     (item_bank, question_labels, abilities[1, :], responses)
 end
 
-function mirt_item_bank_to_full_dummy(item_bank, num_testees, dims; squeeze=false)
+function mirt_item_bank_to_full_dummy(rng, item_bank, num_testees, dims; squeeze=false)
     num_questions = length(item_bank)
     question_labels = [sent() for _ in 1:num_questions]
-    abilities = rand(std_normal, dims, num_testees)
+    abilities = rand(rng, std_normal, dims, num_testees)
     irs = zeros(num_questions, num_testees)
     for question_idx in 1:num_questions
         for testee_idx in 1:num_testees
@@ -65,20 +71,42 @@ function mirt_item_bank_to_full_dummy(item_bank, num_testees, dims; squeeze=fals
             irs[question_idx, testee_idx] = ItemResponse(item_bank, question_idx)(ability)
         end
     end
-    responses = rand(num_questions, num_testees) .< irs
+    responses = rand(rng, num_questions, num_testees) .< irs
     (item_bank, question_labels, abilities, responses)
 end
 
-function dummy_2pl(;num_questions=default_num_questions, num_testees=default_num_testees)
-    item_bank_to_full_dummy(dummy_2pl_item_bank(num_questions), num_testees)
+function dummy_2pl(rng, ;num_questions=default_num_questions, num_testees=default_num_testees)
+    item_bank_to_full_dummy(rng, dummy_2pl_item_bank(rng, num_questions), num_testees)
+end
+dummy_2pl(; kwargs...) = dummy_2pl(Random.default_rng(); kwargs...)
+
+function dummy_3pl(rng ;num_questions=default_num_questions, num_testees=default_num_testees)
+    item_bank_to_full_dummy(rng, dummy_3pl_item_bank(rng, num_questions), num_testees)
+end
+dummy_3pl(; kwargs...) = dummy_3pl(Random.default_rng(); kwargs...)
+
+function dummy_mirt_4pl(rng, dims; num_questions=default_num_questions, num_testees=default_num_testees)
+    mirt_item_bank_to_full_dummy(rng, dummy_mirt_4pl_item_bank(rng, num_questions, dims), num_testees, dims)
+end
+dummy_mirt_4pl(dims; kwargs...) = dummy_mirt_4pl(Random.default_rng(), dims; kwargs...)
+
+function dummy_responses(item_bank::AbstractItemBank, actual_responses, question_idxs::AbstractVector)
+    response_values = Int8.(actual_responses[question_idxs])
+    TrackedResponses(
+        BareResponses(question_idxs, response_values),
+        item_bank,
+        NullAbilityTracker()
+    )
 end
 
-function dummy_3pl(;num_questions=default_num_questions, num_testees=default_num_testees)
-    item_bank_to_full_dummy(dummy_3pl_item_bank(num_questions), num_testees)
+function dummy_responses(rng::AbstractRNG, item_bank, actual_responses, num_questions::Integer)
+    dummy_responses(item_bank, actual_responses, sample(rng, 1:length(actual_responses), num_questions; replace=false))
 end
+dummy_responses(item_bank::AbstractItemBank, actual_responses, num_questions::Integer) = dummy_responses(Random.default_rng(), item_bank, actual_responses, num_questions)
 
-function dummy_mirt_4pl(dims; num_questions=default_num_questions, num_testees=default_num_testees)
-    mirt_item_bank_to_full_dummy(dummy_mirt_4pl_item_bank(num_questions, dims), num_testees, dims)
+function dummy_responses(rng::AbstractRNG, item_bank, actual_responses)
+    dummy_responses(rng, item_bank, actual_responses, round(Int, rand(rng) * length(actual_responses)))
 end
+dummy_responses(item_bank::AbstractItemBank, actual_responses) = dummy_responses(Random.default_rng(), item_bank, actual_responses)
 
 end
