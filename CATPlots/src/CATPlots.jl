bea5d8c9fe4cf8f5cf597f29323e385d2eeeb98c
Frankie Robertson <frankie@robertson.name>
2022-08-12 13:29:40 +0300

    Add perf_plots example

--- CATPlots/src/CATPlots.jl
+++ CATPlots/src/CATPlots.jl
@@ -354,6 +354,90 @@ function Makie.initialize_block!(sg::LabelledToggleGrid, nts::NamedTuple...)
     end
 end
 
+@Block MenuGrid begin
+    @forwarded_layout
+    menus::Vector{Menu}
+    labels::Vector{Label}
+    @attributes begin
+        "The horizontal alignment of the block in its suggested bounding box."
+        halign = :center
+        "The vertical alignment of the block in its suggested bounding box."
+        valign = :center
+        "The width setting of the block."
+        width = Auto()
+        "The height setting of the block."
+        height = Auto()
+        "Controls if the parent layout can adjust to this block's width"
+        tellwidth::Bool = true
+        "Controls if the parent layout can adjust to this block's height"
+        tellheight::Bool = true
+        "The align mode of the block in its parent GridLayout."
+        alignmode = Inside()
+    end
+end
+
+function Makie.initialize_block!(sg::MenuGrid, nts::NamedTuple...)
+    sg.menus = Menu[]
+    sg.labels = Label[]
+
+    for (i, nt) in enumerate(nts)
+        label = haskey(nt, :label) ? nt.label : ""
+        remaining_pairs = filter(pair -> pair[1] ∉ (:label, :format), pairs(nt))
+        menu = Menu(sg.layout[i, 2]; remaining_pairs...)
+        lbl = Label(sg.layout[i, 1], label, halign = :left)
+        push!(sg.menus, menu)
+        push!(sg.labels, lbl)
+    end
+end
+#=
+function initialize_block!(sg::SliderGrid, pairs::Pair...)
+
+    default_format(x) = string(x)
+    default_format(x::AbstractFloat) = string(round(x, sigdigits = 3))
+
+    sg.sliders = Slider[]
+    sg.valuelabels = Label[]
+    sg.labels = Label[]
+
+    extract_label_range_format(pair::Pair) = pair[1], extract_range_format(pair[2])...
+    extract_range_format(p::Pair) = (p...,)
+    extract_range_format(x) = (x, default_format)
+
+    for (i, pair) in enumerate(pairs)
+        label, range, format = extract_label_range_format(pair)
+        l = Label(sg.layout[i, 1], label, halign = :left)
+        slider = Slider(sg.layout[i, 2], range = range)
+        vl = Label(sg.layout[i, 3],
+            lift(x -> apply_format(x, format), slider.value), halign = :right)
+        push!(sg.valuelabels, vl)
+        push!(sg.sliders, slider)
+        push!(sg.labels, l)
+    end
+
+    on(sg.value_column_width) do value_column_width
+        if value_column_width === automatic
+            maxwidth = 0.0
+            for (slider, valuelabel) in zip(sg.sliders, sg.valuelabels)
+                initial_value = slider.value[]
+                a = first(slider.range[])
+                b = last(slider.range[])
+                for frac in (0.0, 0.5, 1.0)
+                    fracvalue = a + frac * (b - a)
+                    set_close_to!(slider, fracvalue)
+                    labelwidth = GridLayoutBase.computedbboxobservable(valuelabel)[].widths[1]
+                    maxwidth = max(maxwidth, labelwidth)
+                end
+                set_close_to!(slider, initial_value)
+            end
+            colsize!(sg.layout, 3, maxwidth)
+        else
+            colsize!(sg.layout, 3, value_column_width)
+        end
+    end
+    notify(sg.value_column_width)
+    return
+end
+=#
 macro automakie()
     quote
 		if "USE_WGL_MAKIE" in keys(ENV)
---
+++ docs/examples/examples/perf_plots.jl
@@ -0,0 +1,125 @@
+#md # Plots for performance of different integrators for ability estimators
+
+using Makie
+import Pkg
+using CSV
+using DataFrames
+using CategoricalArrays: levels
+using IterTools: chain
+using CATPlots
+using CATPlots: LabelledToggleGrid, MenuGrid
+
+@automakie()
+
+df = CSV.read(
+	((@__DIR__) * "/integrator_benchmark_results.csv"),
+	DataFrame;
+	types=Dict(
+		"value" => Float64,
+		"err" => Float64,
+		"bytes" => Int64,
+		"response" => Int32,
+		"item_bank" => Int32,
+		"dim" => Int32,
+		"trial" => Int32,
+		"time" => Float64,
+		"gctime" => Float64,
+		"rtol" => Float64,
+	)
+)
+
+function plot(df)
+	fig = Figure()
+	ax = Axis(fig[1, 1])
+
+	groupables = ["item_bank", "response", "trial", "dim", "integrator"]
+	regressables = ["integrator", "dim", "rtol", "value", "err", "bytes", "gctime"]
+
+	Menu(fig, options = ["none", groupables...], default = "none")
+	toggles = []
+	sliders = []
+	slider_idxs = []
+	menus = []
+	menu_idxs = []
+	for (idx, groupable) in enumerate(groupables)
+		push!(toggles, (
+			label = groupable,
+		))
+		opts = sort(levels(df[!, groupable]))
+		if eltype(opts) <: AbstractString
+			push!(slider_idxs, idx)
+			push!(menus, (
+				label = groupable,
+				options = opts
+			))
+		else
+			push!(menu_idxs, idx)
+			push!(sliders, (
+				label = groupable,
+				range = opts
+			))
+		end
+	end
+	tgrid = LabelledToggleGrid(
+		fig,
+		toggles...,
+		width = 350,
+		tellheight = false
+	)
+	lsgrid = SliderGrid(
+		fig,
+		sliders...,
+		width = 350,
+		tellheight = false
+	)
+	mgrid = MenuGrid(
+		fig,
+		menus...,
+		width = 350,
+		tellheight = false
+	)
+
+	working_df = Observable(nothing)
+	function update_working_df(_)
+		df_prime = df
+		slider_menu_vals = Vector{Any}(undef, length(sliders) + length(menus))
+		slider_menu_vals[slider_idxs] = [slider.value for slider in lsgrid.sliders]
+		slider_menu_vals[menu_idxs] = [menu.selection for menu in mgrid.menus]
+		for (groupable, toggle, value) in zip(groupables, tgrid.toggles, slider_menu_vals)
+			if !toggle.value
+				continue
+			end
+			df_prime = filter(working_df, groupable => value)
+		end
+		working_df[] = df_prime
+	end
+
+	for observable in chain(
+		(t.active for t in tgrid.toggles),
+		(s.value for s in lsgrid.sliders),
+		(m.selection for m in mgrid.menus)
+	)
+		on(update_working_df, observable)
+	end
+
+	x_var_menu = Menu(fig, options = regressables)
+	y_var_menu = Menu(fig, options = regressables)
+	coded_menu = Menu(fig, options = regressables)
+
+	xs = @lift($working_df !== nothing && $(x_var_menu.selection) !== nothing ? $working_df[$(x_var_menu.selection)] : nothing)
+	ys = @lift($working_df !== nothing && $(y_var_menu.selection) !== nothing ? $working_df[$(y_var_menu.selection)] : nothing)
+
+	scatter = @lift(
+		$xs !== nothing && $ys !== nothing && $(coded_menu.selection) !== nothing ?
+			lines!(
+				ax,
+				$xs,
+				$ys;
+				color=$(coded_menu.selection)
+			) : nothing
+	)
+
+	fig
+end
+
+plot(df)
