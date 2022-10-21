0202c26663441b9cf13fe0808d2b0762486c5ebd
Frankie Robertson <frankie@robertson.name>
2022-10-21 19:43:54 +0300

    Silence some difficulty to resolve Aqua warnings

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,15 +29,23 @@ using Random
     )
     Aqua.test_all(
         CATPlots;
-        ambiguities=false
+        ambiguities=false,
+        stale_deps=false # GLMakie is not loaded by CATPlots
+                         # (could possibly be removed as a dependency anyway)
     )
-    # Workaround for https://github.com/JuliaTesting/Aqua.jl/issues/77
-    Aqua.test_ambiguities([
-        ComputerAdaptiveTesting,
-        IRTSupport,
-        CATPlots,
-        Core
-    ])
+    # Ambiguities are not tested in default configuration as a workaround for
+    # https://github.com/JuliaTesting/Aqua.jl/issues/77
+    # Core is not included because of Core.Number, namely
+
+    # CATPlots gets lots of errors from Makie extending Core.Number
+    # Could possibly get some of these fixed in Makie eventually?
+    Aqua.test_ambiguities([CATPlots])
+
+    # ComputerAdaptiveTesting gets errors from FowardDiff extending Core.Number
+    # Could possibly get some of these fixed in ForwardDiff eventually?
+    # https://github.com/JuliaDiff/ForwardDiff.jl/issues/597
+    Aqua.test_ambiguities([ComputerAdaptiveTesting])
+    Aqua.test_ambiguities([IRTSupport])
 end
 
 include("./ability_estimator_1d.jl")
