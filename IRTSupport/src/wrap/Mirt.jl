451a094d8efb9755dfe010c55293e4f2a52bbad3
Frankie Robertson <frankie@robertson.name>
2022-10-21 16:54:18 +0300

    Clean up R/mirt wrapper to use dataframes

--- IRTSupport/src/wrap/Mirt.jl
+++ IRTSupport/src/wrap/Mirt.jl
@@ -13,28 +13,30 @@ function fit_mirt(df; kwargs...)
     library(mirt)
     """
     irt_model = rcall(:mirt, df; kwargs...)
-    R"""
-    do.call(rbind, head(coef($irt_model), -1))
-    """
+    rcopy(
+        R"""
+        coefs_list <- coef($irt_model)
+        coefs_list["GroupPars"] <- NULL
+        coefs_df <- do.call(rbind, coefs_list)
+        coefs_df <- cbind(label = names(coefs_list), coefs_df)
+        as.data.frame(coefs_df)
+        """
+    )
 end
 
 function fit_4pl(df; kwargs...)
     params = fit_mirt(df; model=1, itemtype="4PL", kwargs...)
-    @info "params" params
-    arr = rcopy(Array{Float64}, params)
-    ItemBank4PL(arr[:, 2], arr[:, 1], arr[:, 3], 1.0 .- arr[:, 4])
+    ItemBank4PL(params[!, "d"], params[!, "a1"], params[!, "g"], 1.0 .- params[!, "u"]; labels=params[!, "label"])
 end
 
 function fit_3pl(df; kwargs...)
     params = fit_mirt(df; model=1, itemtype="3PL", kwargs...)
-    arr = rcopy(Array{Float64}, params)
-    ItemBank3PL(arr[:, 2], arr[:, 1], arr[:, 3])
+    ItemBank3PL(params[!, "d"], params[!, "a1"], params[!, "g"]; labels=params[!, "label"])
 end
 
 function fit_2pl(df; kwargs...)
     params = fit_mirt(df; model=1, itemtype="2PL", kwargs...)
-    arr = rcopy(Array{Float64}, params)
-    ItemBank2PL(arr[:, 2], arr[:, 1])
+    ItemBank2PL(params[!, "d"], params[!, "a1"]; labels=labels)
 end
 
 end
