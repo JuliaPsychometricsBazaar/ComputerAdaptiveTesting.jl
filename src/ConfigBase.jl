1eff7c5570cfd6d16ba7552e70b10d398b841daf
Frankie Robertson <frankie@robertson.name>
2023-08-10 08:57:55 +0300

    Remove unused ConfigPurity idea

--- src/ConfigBase.jl
+++ src/ConfigBase.jl
@@ -4,18 +4,7 @@ export CatConfigBase
 
 abstract type CatConfigBase end
 
-# Trait pattern (This block may be heading for the bin)
-abstract type ConfigPurity end
-ConfigPurity(config::CatConfigBase) = ConfigPurity(typeof(config))
-struct PureConfig <: ConfigPurity end
-struct ImpureConfig <: ConfigPurity end
 
-@inline function (self::CatConfigBase)()
-    self(ConfigPurity(self))
-end
 
-@inline function (self::CatConfigBase)(::Type{PureConfig})
-    self
-end
 
 end
--- src/TerminationConditions.jl
+++ src/TerminationConditions.jl
@@ -17,7 +17,6 @@ end
 struct FixedItemsTerminationCondition{} <: TerminationCondition
     num_items::Int64
 end
-ConfigPurity(::FixedItemsTerminationCondition) = PureConfig
 function (condition::FixedItemsTerminationCondition)(responses::TrackedResponses, items::AbstractItemBank)
     length(responses) >= condition.num_items
 end
@@ -25,7 +24,6 @@ end
 struct SimpleFunctionTerminationCondition{F} <: TerminationCondition
     func::F
 end
-ConfigPurity(::SimpleFunctionTerminationCondition) = PureConfig
 function (condition::SimpleFunctionTerminationCondition)(responses::TrackedResponses, items::AbstractItemBank)
     condition.func(responses, items)
 end
