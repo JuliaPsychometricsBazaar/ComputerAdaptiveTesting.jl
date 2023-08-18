b69185c74037fa17eeb2caa3977cf2ea10f8f0aa
Frankie Robertson <frankie@robertson.name>
2023-08-18 14:14:42 +0300

    Move next item aliases

--- src/next_item_rules/NextItemRules.jl
+++ src/next_item_rules/NextItemRules.jl
@@ -143,25 +143,6 @@ function (item_criterion::ItemCriterion)(tracked_responses, item_idx)
     item_criterion(criterion_state, tracked_responses, item_idx)
 end
 
-"""
-This mapping provides next item rules through the same names that they are
-available through in the `catR` R package. TODO compability with `mirtcat`
-"""
-const catr_next_item_aliases = Dict(
-    "MFI" => (ability_estimator; parallel=true) -> ItemStrategyNextItemRule(ExhaustiveSearch1Ply(parallel), InformationItemCriterion(ability_estimator)),
-    "bOpt" => (ability_estimator; parallel=true) -> ItemStrategyNextItemRule(ExhaustiveSearch1Ply(parallel), UrryItemCriterion(ability_estimator)),
-    #"thOpt",
-    #"MLWI",
-    #"MPWI",
-    #"MEI",
-    "MEPV" => (ability_estimator; parallel=true) -> ItemStrategyNextItemRule(ExhaustiveSearch1Ply(parallel), ExpectationBasedItemCriterion(ability_estimator, AbilityVarianceStateCriterion(ability_estimator))),
-    #"progressive",
-    #"proportional",
-    #"KL",
-    #"KLP",
-    #"GDI",
-    #"GDIP",
-    #"random"
-)
+include("./aliases.jl")
 
 end
---
+++ src/next_item_rules/aliases.jl
@@ -0,0 +1,20 @@
+"""
+This mapping provides next item rules through the same names that they are
+available through in the `catR` R package. TODO compability with `mirtcat`
+"""
+const catr_next_item_aliases = Dict(
+    "MFI" => (ability_estimator; parallel=true) -> ItemStrategyNextItemRule(ExhaustiveSearch1Ply(parallel), InformationItemCriterion(ability_estimator)),
+    "bOpt" => (ability_estimator; parallel=true) -> ItemStrategyNextItemRule(ExhaustiveSearch1Ply(parallel), UrryItemCriterion(ability_estimator)),
+    #"thOpt",
+    #"MLWI",
+    #"MPWI",
+    #"MEI",
+    "MEPV" => (ability_estimator; parallel=true) -> ItemStrategyNextItemRule(ExhaustiveSearch1Ply(parallel), ExpectationBasedItemCriterion(ability_estimator, AbilityVarianceStateCriterion(ability_estimator))),
+    #"progressive",
+    #"proportional",
+    #"KL",
+    #"KLP",
+    #"GDI",
+    #"GDIP",
+    #"random"
+)
