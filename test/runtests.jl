eccb4e736e2d46d0399e4275b8a166181398cb97
Frankie Robertson <frankie@robertson.name>
2023-08-20 12:11:14 +0300

    Switch from Test to XUnit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,19 @@
-using Test
+using XUnit
 
-include("./utils.jl")
 
-using .CATTestUtils: @namedtestset
 
-@namedtestset aqua "Aqua automated quality checks" begin
+@testset "aqua" begin
     include("./aqua.jl")
 end
 
-@namedtestset ability_estimator_1d "1-dimensional ability estimators" begin
+@testset "abilest_1d" begin
     include("./ability_estimator_1d.jl")
 end
 
-@namedtestset ability_estimator_2d "1-dimensional ability estimators" begin
+@testset "abilest_2d" begin
     include("./ability_estimator_2d.jl")
 end
 
-@namedtestset smoke "Smoke tests" begin
+@testset "smoke" begin
     include("./smoke.jl")
 end
