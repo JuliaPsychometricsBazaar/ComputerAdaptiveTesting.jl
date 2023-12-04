1eaa5bd534ca8f014940e34e072def251b919ba2
Frankie Robertson <frankie@robertson.name>
2023-12-04 09:30:42 +0200

    Remove CATPlots dependency from test dependencies

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,24 +1,13 @@
 using Aqua
-using CATPlots
 using ComputerAdaptiveTesting
 
 Aqua.test_all(
     ComputerAdaptiveTesting;
     ambiguities=false,
 )
-Aqua.test_all(
-    CATPlots;
-    ambiguities=false,
-    stale_deps=false # GLMakie is not loaded by CATPlots
-                     # (could possibly be removed as a dependency anyway)
-)
 # Ambiguities are not tested in default configuration as a workaround for
 # https://github.com/JuliaTesting/Aqua.jl/issues/77
 # Core is not included because of Core.Number, namely
-
-# CATPlots gets lots of errors from Makie extending Core.Number
-# Could possibly get some of these fixed in Makie eventually?
-Aqua.test_ambiguities([CATPlots])
 
 # ComputerAdaptiveTesting gets errors from FowardDiff extending Core.Number
 # Could possibly get some of these fixed in ForwardDiff eventually?
