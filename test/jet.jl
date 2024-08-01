491b3aa6a108f35bb30a1aa12a54c0823749d8e2
Frankie Robertson <frankie@robertson.name>
2024-08-01 19:38:32 +0300

    Use typo mode for JET --- more realistic since interfaces aren't supported yet

--- test/jet.jl
+++ test/jet.jl
@@ -1,8 +1,18 @@
 using JET
+using Optim: Optim
 
 @testset "JET checks" begin
-    rep = report_package(ComputerAdaptiveTesting)
+    rep = report_package(
+        ComputerAdaptiveTesting;
+        ignored_modules=(
+            ComputerAdaptiveTesting.Comparison,
+            ComputerAdaptiveTesting.PushVectors,
+            Base.Broadcast,
+            Optim
+        ),
+        mode=:typo
+    )
     @show rep
-    @test length(JET.get_reports(rep)) <= 43
+    @test length(JET.get_reports(rep)) == 5
     @test_broken length(JET.get_reports(rep)) == 0
 end
