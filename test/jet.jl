36846a2fb4623a39bc782cab4558e53b6c598244
Frankie Robertson <frankie@robertson.name>
2024-08-20 16:00:58 +0300

    Make JET only check this module

--- test/jet.jl
+++ test/jet.jl
@@ -4,15 +4,11 @@ using Optim: Optim
 @testset "JET checks" begin
     rep = report_package(
         ComputerAdaptiveTesting;
-        ignored_modules = (
-            ComputerAdaptiveTesting.Comparison,
-            ComputerAdaptiveTesting.PushVectors,
-            Base.Broadcast,
-            Optim
+        target_modules = (
+            ComputerAdaptiveTesting,
         ),
         mode = :typo
     )
     @show rep
-    @test length(JET.get_reports(rep)) <= 5
-    @test_broken length(JET.get_reports(rep)) == 0
+    @test length(JET.get_reports(rep)) == 0
 end
