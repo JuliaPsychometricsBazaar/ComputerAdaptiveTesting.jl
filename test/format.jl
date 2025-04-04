e6c8d405fd66a06ebdc59b9d7e60088e91fef2e8
Frankie Robertson <frankie@robertson.name>
2025-04-04 13:54:43 +0300

    Remove formatting test

--- test/format.jl
+++
@@ -1,8 +0,0 @@
-using JuliaFormatter
-using ComputerAdaptiveTesting
-
-@testset "format" begin
-    dir = pkgdir(ComputerAdaptiveTesting)
-    @test format(dir * "/src"; overwrite = false)
-    @test format(dir * "/test"; overwrite = false)
-end
